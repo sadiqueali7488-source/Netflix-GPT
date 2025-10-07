42c057001d2d3b4b3a2f643dfdc9f4f9e0acfcc4
Sadique Ali <sadiqueali7488@gmail.com>
2025-10-07 15:00:03 +0530

    added movies poster

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -5,14 +5,16 @@ const MovieList = ({ title, movies }) => {
   console.log(movies);
 
   return (
-    <div>
-      <div>
-        <h1>{title}</h1>
+    <div className="px-6">
+      <h1 className="text-3xl py-4 text-white">{title}</h1>
+      <div className="flex overflow-x-hidden">
+        
 
-        <div>
+        <div className="flex gap-4 hide-scroll-bar">
           {movies.map((movie) => (
             <MovieCard key={movie?.id} posterPath={movie?.poster_path} />
           ))}
+          
         </div>
       </div>
     </div>
--- src/Components/SecondaryContainer.jsx
+++ src/Components/SecondaryContainer.jsx
@@ -4,9 +4,20 @@ import { useSelector } from "react-redux";
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
-    <div>
-      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-    </div>
+    movies?.nowPlayingMovies && (
+      <div className=" bg-black">
+        <div className="-mt-52 pl-12 relative z-20">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
+          <MovieList
+            title={"Upcoming Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
+    )
   );
 };
 
